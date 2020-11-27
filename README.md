79eff1f5cf60c2f5b8af1de2bd297ad7d56f33e6
mooyeon-choi <dus1208@ajou.ac.kr>
2020-11-27 10:51:47 +0900

    Add project

---
+++ README.md
@@ -0,0 +1,41 @@
+# Habit tracker
+
+## About The Project
+
+<img src="./images/page.PNG" alt="Page" style="zoom: 67%;" />
+
+ 취미 목록 추가, 각 취미별 횟수 조절, 횟수가 1 이상인 취미의 갯수 표시, 목록 삭제 기능을 만들어보며 react의 활용법을 공부한 프로젝트 입니다.
+
+## Built With
+
+* React
+* Fontawesome
+
+## Getting Started
+
+ 로컬환경에서 프로젝트를 실행시켜보기 위한 방법입니다.
+
+### Prerequisites
+
+ npm, yarn module이 사용됩니다.
+
+* npm 설치
+
+  [node.js 공식사이트](https://nodejs.org/en/)
+
+* yarn 설치
+
+  ```bash
+  npm install yarn --global
+  ```
+
+### Installation
+
+* Install YARN packages
+
+  ```bash
+  yarn install
+  ```
+
+
+
---
+++ src/components/habits.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Habit from './habit';
+import HabitAddForm from './habitAddForm';
+
+class Habits extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0},
+      { id: 2, name: 'Running', count: 0},
+      { id: 3, name: 'Coding', count: 0},
+    ]
+  }
+  
+  handleIncrement = habit => {
+    this.props.onIncrement(habit);
+  }
+
+  handleDecrement = habit => {
+    this.props.onDecrement(habit);
+  }
+
+  handleDelete = habit => {
+    this.props.onDelete(habit);
+  }
+
+  handleAdd = name => {
+    this.props.onAdd(name);
+  }
+
+  render() {
+    return (
+      <>
+      <HabitAddForm onAdd={this.handleAdd}/>
+      <ul>
+        {this.props.habits.map(habit => 
+          <Habit 
+            key={habit.id} 
+            habit={habit} 
+            onIncrement={this.handleIncrement} 
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        )}
+      </ul>;
+      <button className="habits-reset" onClick={this.props.onReset}>
+        Reset All
+      </button>
+      </>
+    )
+  }
+}
+
+export default Habits;
